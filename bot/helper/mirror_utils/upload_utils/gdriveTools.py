4d0d1e2e2ec5337498cb5a814176ec9c7fc799fc
Tharindu Prabath <116081100+tharindu899@users.noreply.github.com>
2023-03-09 20:48:30 +0530

    update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -732,9 +732,9 @@ class GoogleDriveHelper:
             ulist_listener[self.user_id] = [[fileName, contents_count, itemType], telemsg]
             buttons = ButtonMaker()
             if len(telemsg) > 1:
-                buttons.sbutton('⌫', f"cari {self.user_id} changepg -1")
-                buttons.sbutton(f'Pᴀɢᴇs\n1 / {len(telemsg)}', f"cari {self.user_id} pagnav 0")
-                buttons.sbutton('⌦', f"cari {self.user_id} changepg 1")
+                buttons.sbutton('◀️', f"cari {self.user_id} changepg -1")
+                buttons.sbutton(f'📖Pᴀɢᴇs\n1 / {len(telemsg)}', f"cari {self.user_id} pagnav 0")
+                buttons.sbutton('▶️', f"cari {self.user_id} changepg 1")
             buttons.sbutton('❌️Close', f"cari {self.user_id} clo", 'footer')
             extra = f'''╭ <b>🥶Query :</b> <i>{fileName}</i>
 ├ <b>📚Total Results :</b> <i>{contents_count}</i>
